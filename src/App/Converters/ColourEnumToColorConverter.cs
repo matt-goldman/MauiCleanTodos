ef2ff6fa29c233b6fc4a5cbfcdad41032070d2f8
Matt Goldman <matt.goldman@outlook.com>
2022-08-22 08:06:34 +1000

    Started adding colour picker

---
+++ src/App/Converters/ColourEnumToColorConverter.cs
@@ -0,0 +1,37 @@
+﻿using System.Globalization;
+using MauiCleanTodos.Shared.TodoLists;
+
+namespace MauiCleanTodos.App.Converters;
+public class ColourEnumToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var colour = (Colours)value;
+
+        switch (colour)
+        {
+            case Colours.Orange:
+                return Colors.Orange;
+            case Colours.Purple:
+                return Colors.Purple;
+            case Colours.Blue:
+                return Colors.Blue;
+            case Colours.Yellow:
+                return Colors.Yellow;
+            case Colours.White:
+            default:
+                return Colors.White;
+            case Colours.Red:
+                return Colors.Red;
+            case Colours.Green:
+                return Colors.Green;
+            case Colours.Grey:
+                return Colors.Grey;
+        }
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        throw new NotImplementedException();
+    }
+}
--- src/App/PopupPages/NewListPopup.xaml.cs
+++ src/App/PopupPages/NewListPopup.xaml.cs
@@ -1,11 +1,31 @@
+using System.Collections.ObjectModel;
 using CommunityToolkit.Maui.Views;
+using MauiCleanTodos.Shared.TodoLists;
 
 namespace MauiCleanTodos.App.PopupPages;
 
 public partial class NewListPopup : Popup
 {
+	public ObservableCollection<test> Colours { get; set; } = new();
+
 	public NewListPopup()
 	{
 		InitializeComponent();
+		BindingContext = this;
+		
+		foreach(var colour in (Colours[])Enum.GetValues(typeof(Colours)))
+		{
+			Colours.Add(new test { Colour = colour});
+		}
 	}
+
+	private void ColoursSelectionChanged(object sender, SelectionChangedEventArgs e)
+	{
+
+    }
+}
+
+public class test
+{
+	public Colours Colour { get; set; }
 }
