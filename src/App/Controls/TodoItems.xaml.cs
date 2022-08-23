48d1947d089e3464186635f917e216fd7ab5d758
Matt Goldman <matt.goldman@outlook.com>
2022-08-23 20:49:31 +1000

    Started adding bottom sheet

---
+++ src/App/Controls/TodoItems.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiApp1;
+
+public partial class TodoItems : ContentView
+{
+	public TodoItems()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/App/Platforms/Android/PageExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Google.Android.Material.BottomSheet;
+using Microsoft.Maui.Platform;
+
+namespace BottomSheet;
+
+public static partial class PageExtensions
+{
+    public static void ShowBottomSheet(this Page page, IView bottomSheetContent, bool dimDismiss)
+    {
+        var bottomSheetDialog = new BottomSheetDialog(Platform.CurrentActivity?.Window?.DecorView.FindViewById(Android.Resource.Id.Content)?.RootView?.Context);
+        bottomSheetDialog.SetContentView(bottomSheetContent.ToPlatform(page.Handler?.MauiContext ?? throw new Exception("MauiContext is null")));
+        bottomSheetDialog.Show();
+    }
+}
