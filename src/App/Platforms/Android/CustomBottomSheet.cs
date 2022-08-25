369bbfcaa580d6d8d091c1cc060921d7f0079893
Matt Goldman <matt.goldman@outlook.com>
2022-08-26 09:07:17 +1000

    Fixed scrolling list collection Trying to style bottom sheet

---
+++ src/App/Platforms/Android/CustomBottomSheet.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Google.Android.Material.BottomSheet;
+
+namespace MauiCleanTodos.App.Platforms.Android;
+public class CustomBottomSheet : BottomSheetDialog
+{
+    public CustomBottomSheet(Context context) : base(context)
+    {
+    }
+
+    protected override void OnCreate(Bundle savedInstanceState)
+    {
+        base.OnCreate(savedInstanceState);
+
+    }
+}
