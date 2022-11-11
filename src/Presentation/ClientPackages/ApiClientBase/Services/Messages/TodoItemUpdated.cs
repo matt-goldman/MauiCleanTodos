adcf0730e405477887af47ff4207c1cd699b4079
Matt Goldman <matt.goldman@outlook.com>
2022-11-12 09:53:40 +1100

    removed unused messages

--- src/Presentation/ClientPackages/ApiClientBase/Services/Messages/TodoItemUpdated.cs
+++
@@ -1,9 +0,0 @@
-﻿using CommunityToolkit.Mvvm.Messaging.Messages;
-
-namespace MauiCleanTodos.ApiClient.Services.Messages;
-public class TodoItemUpdated : ValueChangedMessage<TodoItemDto>
-{
-    public TodoItemUpdated(TodoItemDto value) : base(value)
-    {
-    }
-}
