989d2e51d0c060a49cf08fc8b1c54c142b8d0816
Matt Goldman <matt.goldman@outlook.com>
2022-11-12 09:25:13 +1100

    Attempted to set transparent background on Android bottom sheet

--- src/Presentation/App/Platforms/Android/PageExtensions.cs
+++ src/Presentation/App/Platforms/Android/PageExtensions.cs
@@ -9,6 +9,9 @@ public static partial class PageExtensions
     {
         var bottomSheetDialog = new BottomSheetDialog(Platform.CurrentActivity?.Window?.DecorView.FindViewById(Android.Resource.Id.Content)?.RootView?.Context);
         bottomSheetDialog.SetContentView(bottomSheetContent.ToPlatform(page.Handler?.MauiContext ?? throw new Exception("MauiContext is null")));
+        bottomSheetDialog.Window.SetDimAmount(0);
+        bottomSheetDialog.Behavior.Hideable = dimDismiss;
+        bottomSheetDialog.Behavior.FitToContents = true;
         bottomSheetDialog.Show();
     }
 }
